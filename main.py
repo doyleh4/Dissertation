a60efc2f0b6041fdda243cf8aaa01270c9e73493
doyleh4 <60394085+doyleh4@users.noreply.github.com>
2022-10-31 16:34:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,201 @@
+# Install packages
+# OS
+import argparse
+import sys
+
+# Essenital
+import cv2 as cv
+import numpy as np
+# Helpers
+from scipy.interpolate import splprep, splev
+
+# Retrieve input file from run-time ram
+parser = argparse.ArgumentParser(description='This program shows how to use background subtraction methods provided by \
+                                              OpenCV. You can process both videos and images.')
+parser.add_argument('--input', type=str, help='Path to a video or a sequence of image.',
+                    default="./videos/samples/sample0.mov")
+args = parser.parse_args()
+
+print("Retrieiving video from files")
+
+capture = cv.VideoCapture(args.input)
+
+if not capture.isOpened():
+    print("File failed to open, there is a file path error.")
+    sys.exit(0)
+
+subtraction = cv.createBackgroundSubtractorMOG2()
+
+
+# TODO: Classify all similar functions
+
+def find_corners(grayframe):
+    """
+    Function to find the corners in the frame. Influenced by:
+    https://docs.opencv.org/3.4/d4/d8c/tutorial_py_shi_tomasi.html
+    :param grayframe:
+    :return:
+    """
+    corners = cv.goodFeaturesToTrack(grayframe, 100, 0.1, 15)
+    corners = np.int0(corners)
+
+    return corners
+
+
+def kmeans_clustering(frame):
+    Z = frame.reshape((-1, 3))
+    # convert to np.float32
+    Z = np.float32(Z)
+    # define criteria, number of clusters(K) and apply kmeans()
+    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    K = 5
+    ret, label, center = cv.kmeans(Z, K, None, criteria, 10, cv.KMEANS_RANDOM_CENTERS)
+
+    # Now convert back into uint8, and make original image
+    center = np.uint8(center)
+    res = center[label.flatten()]
+    res2 = res.reshape(frame.shape)
+    # cv.imshow('res2', res2)
+    res2 = cv.cvtColor(res2, cv.COLOR_BGR2GRAY)
+    return res2
+
+
+def bin_threshold(frame):
+    ret, thresh = cv.threshold(frame, 90, 255, cv.THRESH_BINARY_INV)
+
+    kernel = np.ones((2, 2), np.uint8)
+    thresh = cv.erode(thresh, kernel, iterations=1)
+    kernel = np.ones((2, 2), np.uint8)
+    thresh = cv.morphologyEx(thresh, cv.MORPH_OPEN, kernel)
+    kernel = np.ones((5, 5), np.float32) / 25
+    thresh = cv.filter2D(thresh, -1, kernel)
+
+    # g_frame = frame.copy()
+    # result = cv.bitwise_and(g_frame, thresh)
+
+    contours, _ = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+    temp = frame.copy()
+    # for index in range(len(contours)):
+    #     contour = contours[index]
+    #     approx = cv.approxPolyDP(contour, 0.01 * cv.arcLength(contour, True), True)
+    #     # if len(approx) is 3:
+    #     #     print("hit")
+    #     if 10 < len(approx) < 25:
+    #         temp = cv.drawContours(thresh, [approx], 0, 65, 3)
+    #     # print(temp)
+    #     # if 40 < len(contours[index]) < 100:
+    #     #     temp = cv.drawContours(thresh, contours, index, 145, 3)
+
+    smoothened = []
+    for contour in contours:
+        x, y = contour.T
+
+        x = x.tolist()[0]
+        y = y.tolist()[0]
+
+        tck, u = splprep([x, y], u=None, s=1.0, per=1)
+
+        u_new = np.linspace(u.min(), u.max(), 25)
+
+        x_new, y_new = splev(u_new, tck, der=0)
+
+        res_array = [[[int(i[0]), int(i[1])]] for i in zip(x_new, y_new)]
+        smoothened.append(np.asarray(res_array, dtype=np.int32))
+
+        # approx = cv.approxPolyDP(smoothened, 0.01 * cv.arcLength(smoothened, True), True)
+
+        temp = cv.drawContours(temp, smoothened, -1, 255, 2)
+
+    return temp
+
+
+def find_clubhead(frame):
+    result = kmeans_clustering(frame)
+    cv.imshow("Cluserting", result)
+    contours = bin_threshold(result)
+    cv.imshow("Contours", contours)
+
+    return contours
+
+
+def optical_flow(frame, is_colour):
+    grayframe = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    temp_frame = frame.copy()
+
+    corners = find_corners(grayframe)
+    clubhead = find_clubhead(frame)
+    # clubhead = np.uint16(np.around(clubhead))
+    #
+    # for i in clubhead[0, :]:
+    #     # draw the outer circle
+    #     cv.circle(temp_frame, (i[0], i[1]), i[2], (0, 255, 0), 2)
+    #     # draw the center of the circle
+    #     cv.circle(temp_frame, (i[0], i[1]), 2, (0, 0, 255), 3)
+    for i in corners:
+        x, y = i.ravel()
+        cv.circle(temp_frame, (x, y), 3, 255, -1)
+
+    # cv.imshow("Detected Clubhead", clubhead)
+    cv.imshow("Frame", frame)
+    # cv.waitKey(0)
+    return temp_frame
+
+
+def background_subtraction(frame, is_colour):
+    """
+    Execute background substitution on the frame
+    :return: retrun the resulting frame
+    """
+    # Assign vars and pick background subtraction method
+    g_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    # Apply background substitution
+    fg_mask = subtraction.apply(g_frame)
+    ret, fg_mask = cv.threshold(fg_mask, 127, 255, cv.THRESH_BINARY)
+
+    cv.imshow("Foreground Mask", fg_mask)
+    if is_colour:
+        return cv.bitwise_and(frame, frame, mask=fg_mask)
+    else:
+        return cv.bitwise_and(g_frame, fg_mask)
+
+
+def main_loop():
+    while capture.isOpened():
+        ret, frame = capture.read()
+        # if frame is None:
+        #     break
+
+        # if frame is read correctly ret is True
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+
+        frame = cv.resize(frame, (int(frame.shape[1] / 2), int(frame.shape[0] / 2)))
+        frame = cv.rotate(frame, cv.ROTATE_180)
+
+        temp_frame = frame.copy()
+        # fg_frame = background_subtraction(temp_frame, True)
+        # cv.imshow('Foreground', fg_frame)
+        # cv.imshow("Foreground frame", of_frame)
+
+        # of_frame = optical_flow(frame, True)
+        of_frame = optical_flow(temp_frame, True)
+
+        cv.imshow('Optical Flow Frame', of_frame)
+        cv.imshow('Frame', frame)
+
+        print(capture.get(1))
+        if (capture.get(1) == 50.0) or (capture.get(1) == 75.0) or (capture.get(1) == 80.0):
+            print("yep")
+            cv.waitKey(500000)
+
+        keyboard = cv.waitKey(30)
+        if keyboard == 'q' or keyboard == 27:
+            break
+
+
+if __name__ == "__main__":
+    # play the video, main loop
+    main_loop()
