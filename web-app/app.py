34f311416f1fa98b694f9f6d5a297d5e693d0b1f
doyleh4 <60394085+doyleh4@users.noreply.github.com>
2023-03-15 19:28:05 +0000

    Implemented drag and drop video upload. Still need to process the uploaded video and display results

--- web-app/app.py
+++ web-app/app.py
@@ -1,13 +1,38 @@
-from flask import Flask, render_template, Response, send_file
+import os
+
+from flask import Flask, render_template, Response, send_file, request, jsonify
 
 from main import face_on_view
 
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 200  # 200 MB limit
 
 
 @app.route('/')
 def index():
+    # TODO: drag and drop box that will send video to server on upload this will involve updating the new file paths
+    #  in the source code to the new uploaded file and send the correct files to the static folder
     return render_template("index.html")
+
+
+@app.route("/upload_video", methods=["POST"])
+def upload_video():
+    # This function saves the uploaded video to repo in server
+    tags = ["FO-video", "DTL-video"]
+    for tag in tags:
+        try:
+            file = request.files.get(tag)
+            if file:
+                try:
+                    print(os.getcwd())
+                    file.save(file.filename)
+                    print("Downloaded to server")
+                except Exception as e:
+                    print("ERRRRRRORORRRRR")
+                    print(e)
+        except:
+            continue
+    return jsonify({'success': True})
 
 
 # Route to serve the video file
@@ -24,6 +49,7 @@ def video(filename):
 
 @app.route('/FO_analysis')
 def fo_analysis():
+    # TODO: Synchronise video
     # width, height = face_on_view()
     width, height = (480, 860)
     return render_template("FO_analysis.html")
@@ -31,6 +57,7 @@ def fo_analysis():
 
 @app.route("/DTL_analysis")
 def dtl_analysis():
+    # TODO: Synchronise video
     # width, height = down_the_line()
     width, height = (480, 860)
     return render_template("DTL_analysis.html")
