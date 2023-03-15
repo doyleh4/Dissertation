55f07f19d62f7b93265ea5f5ee10042254586096
doyleh4 <60394085+doyleh4@users.noreply.github.com>
2023-03-15 19:29:30 +0000

    forogot a few files form previous commit

---
+++ web-app/static/js/upload_video.js
@@ -0,0 +1,26 @@
+function uploadFiles() {
+    console.log("Upload files to server")
+    var foFile = document.getElementById('fo-upload').files[0];
+    var dtlFile = document.getElementById('dtl-upload').files[0];
+
+    /* TODO: Update his to have a check to see if both files are filled */
+    handleDownload(foFile, dtlFile)
+
+
+    console.log("Files saves to server")
+}
+
+function handleDownload(file, file2){
+    let form = new FormData()
+
+    form.append("FO-video", file)
+    form.append("DTL-video", file)
+
+    fetch("/upload_video", {
+        method : "POST",
+        body : form
+    })
+    .then(response => response.json())
+    .then(data => { console.log("Video download successful")})
+    .catch(error => { console.log("Video failed to download")})
+}
